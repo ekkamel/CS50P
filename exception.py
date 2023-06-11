7cdd203fdefdd10386be4752948d540ce320d6c7
ekkamel <ekkamel@gmail.com>
2023-06-11 22:17:03 +0300

    added exception module

--- exception.py
+++ exception.py
@@ -1,7 +1,23 @@
-try: 
-    x = int(input("What is x? "))
+def main(): 
+    x = get_int()
     print(f"x is {x}")
-except ValueError: 
-    print("x is not am integer")
 
 
+
+def get_int(): 
+
+    while True:
+        try: 
+            x = int(input("What is x? "))
+    
+        except ValueError: 
+            print("x is not am integer")
+
+        else:
+            break
+    
+    return x   ## Or just omit the break and make it ELSE /n return x
+
+
+
+main()
