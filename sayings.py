9fb69623caee8547383f572846cf367d4c20a92f
ekkamel <ekkamel@gmail.com>
2023-06-22 23:12:47 +0300

    called api, created package and called it

---
+++ sayings.py
@@ -0,0 +1,21 @@
+def main(): 
+    hello("world")
+    goodbye("world")
+
+
+def hello(name): 
+    print(f"hello, {name}")
+
+
+
+def goodbye(name): 
+    print(f"goodbye, {name}")
+
+
+## Do not call main() unconditionally, use this technique instead 
+## this way if I run the code from command line it will print the 
+## Hello and Goodbye world
+## however, if called from another prgram will rin only the parameter
+## that is passed to it
+if __name__ == "__main__":
+    main()
