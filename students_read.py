2d9da5ceec32fc91b044290f196c38f11ee504c2
ekkamel <ekkamel@gmail.com>
2023-07-02 00:27:33 +0300

    added csv dictionary

--- students_read.py
+++ students_read.py
@@ -1,8 +1,12 @@
-def main():
-    print_unsorted()
-    print_sorted()
-    create_dict()
+import csv 
 
+def main():
+    #print_unsorted()
+    #print_sorted()
+    create_dict()
+    lambda_function()
+    use_csv_module()
+    use_csv_dictreader()
 
 def print_unsorted():    
     with open("students.csv") as file: 
@@ -29,19 +33,62 @@ def create_dict():
     students = []
     with open("students.csv") as file: 
         for line in file: 
-            name, house = line.rstrip().split(",")
+            name, house, score = line.rstrip().split(",")
             student = {}
             student["name"] = name 
             student["house"] = house
-            ## or student = {"name": name, "house": house}
-            students.append(student)
+            student["score"] = score
+            ## or student = {"name": name, "house": house, "score": score}
+            students.append(student)    
     
-    for student in students: 
-        print(f"{student['name']} is in {student['house']}")
+        
+        ## to sort on the house (use it as KEY for sorting)
+
+        def get_score(student): 
+            return int(student["score"])
+
+        for student in sorted(students, key=get_score, reverse=True): 
+            print(f"{student['name']} is in {student['house']} with score {student['score']}")
 
 
+def lambda_function(): 
+    students = []
+    with open("students.csv") as file: 
+        for line in file: 
+            name, house, score = line.rstrip().split(",")
+            student = {}
+            student["name"] = name
+            student["house"] = house
+            student["score"] = score
+            students.append(student)
+
+        for student in sorted(students, key=lambda student: int(student["score"])):
+            print(f"{student['name']} lives in {student['house']} with score {student['score']}")
+
+        ## student: is not the name, lambda functions do not have names
+        ## student: is the entity that the function is working on 
+        ## student: is the function parameter
+
+def use_csv_module():
+    students = []
+    with open("students2.csv") as file: 
+        reader = csv.reader(file)
+        for row in reader:
+            students.append({"name": row[0], "score": int(row[1]), "home": row[2]})
+
+        for student in sorted(students, key=lambda student: int(student["score"])): 
+            print(f"{student['name']} lives in {student['home']} with score {student['score']}")     
 
 
+def use_csv_dictreader():
+    students = []
+    with open("students3.csv") as file: 
+        reader = csv.DictReader(file)  ## Note the first row in the csv file
+        for row in reader: 
+            students.append({"name": row["name"], "home": row["home"]})
+        
+        for student in sorted(students, key=lambda student: student["name"]): 
+            print(f"{student['name']} lives in {student['home']}")     
 
 
 
